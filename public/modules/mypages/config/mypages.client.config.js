b88669740318826edcc76e39a5cca2df2ba6e0ac
jindev <jin@jinui-MacBook-Air.local>
2015-06-27 02:33:56 +0900

    file upload

--- public/modules/mypages/config/mypages.client.config.js
+++ public/modules/mypages/config/mypages.client.config.js
@@ -4,8 +4,8 @@
 angular.module('mypages').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		//Menus.addMenuItem('topbar', 'Mypages', 'mypages', 'dropdown', '/mypages(/create)?');
-		Menus.addSubMenuItem('topbar', 'mypages', 'List Mypages', 'mypages');
-		Menus.addSubMenuItem('topbar', 'mypages', 'New Mypage', 'mypages/create');
+		Menus.addMenuItem('topbar', 'Mypages', 'mypages');
+		//Menus.addSubMenuItem('topbar', 'mypages', 'List Mypages', 'mypages');
+		//Menus.addSubMenuItem('topbar', 'mypages', 'New Mypage', 'mypages/create');
 	}
 ]);
